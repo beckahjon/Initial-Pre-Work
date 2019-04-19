985135999f256673aa827a030096cf16afc80cd8
Beckah <rebekah.whittaker19@gmail.com>
2019-04-19 10:30:50 -0600

    Assign variable a and b

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,8 @@
+//I learned that you can store a value in a variable using the assignment operator. Assignment goes from right to left, which means everything to the rigth of the equal sign is resolved before the value is assigned to the variable to the left.
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+a = 7;
+b = a;
