424f42d945360299f1f25fe93b2865ede78f3229
Beckah <rebekah.whittaker19@gmail.com>
2019-04-19 10:59:10 -0600

    Use /= to divide numbers from variables

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,10 @@
+//You can divide a variable by a number by using the forward slash symbol combined with the equal sign.
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
