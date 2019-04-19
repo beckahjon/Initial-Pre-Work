be4a4e1fd3b491c72f2ec054deef5f5291905b4d
Beckah <rebekah.whittaker19@gmail.com>
2019-04-19 17:02:22 -0600

    Add single quotation marks and remove backslash

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,2 @@
+//In JavaScript you can use either single or double quotation marks, as long as you begin and end with the same. You might want to use both in a string, to have a conversation in quotes or to save an <a> tag in quotes.
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
