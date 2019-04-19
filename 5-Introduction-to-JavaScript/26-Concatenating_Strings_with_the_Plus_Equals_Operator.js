9ed712a15e883ba2754b39e80a86aacdcae33bd7
Beckah <rebekah.whittaker19@gmail.com>
2019-04-19 17:05:33 -0600

    Use += operator to concatenate multiple strings

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,9 @@
+//It is possible to concatenate a string onto the end of an existing string by using the += operator. This can help break up long strings.
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
