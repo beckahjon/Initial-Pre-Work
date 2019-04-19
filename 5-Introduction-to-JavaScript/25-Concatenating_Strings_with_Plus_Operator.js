4fd77c19594b981de809f50266243282b78ef0bc
Beckah <rebekah.whittaker19@gmail.com>
2019-04-19 17:04:53 -0600

    Concatenate string using + operator

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,7 @@
+//To concatenate is to link things together. You can use the + operator with a string value to concatenate things together. You need to add your own spaces to concatenated strings.
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
