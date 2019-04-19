d43367f190e429e011fdb014b0076f8fa97b688d
Beckah <rebekah.whittaker19@gmail.com>
2019-04-19 10:32:39 -0600

    Correct camelCase in all variables

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,10 @@
+//Capitalization matters in JavaScript, because all variables and function names are case sensitive. You can have multiple variables with the same name but different casing. It is best practice to use camelCase, in which the first word is all lowercase and the first letter of each subsequent word is capitalized.
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
