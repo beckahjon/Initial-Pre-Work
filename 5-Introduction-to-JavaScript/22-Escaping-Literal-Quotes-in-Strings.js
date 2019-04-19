1f8a2e40ac59c38711933994975a2d7bd2e5bdf9
Beckah <rebekah.whittaker19@gmail.com>
2019-04-19 17:01:53 -0600

    Use backslash to escape direct quote within string

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,2 @@
+//If you need to use a literal quote inside your string, you can use a backslash in front of it to escape JavaScript from considering it the end of the string.
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
