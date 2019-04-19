0d7bc9050003ae46564f96ec80a93d9411fa4f3d
Beckah <rebekah.whittaker19@gmail.com>
2019-04-19 10:29:29 -0600

    Add variable of myName

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,6 @@
+//JavaScript uses seven different data types to create data that is meaningful to the computer. Variables can store and manipulate data by using a label to point to the data. Any of the seven data types can be stored in a variable.
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
