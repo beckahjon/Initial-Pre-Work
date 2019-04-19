b699055ef66bc50a07ebc94bbdf32176654fdacd
Beckah <rebekah.whittaker19@gmail.com>
2019-04-19 17:07:10 -0600

    Use + operator to insert variables to existing string

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+//You can piece together a string by using the + operator to insert one or more variables to an existing string.
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Beckah Whittaker";
+var myStr = "My name is " + myName + ", how are you?";
