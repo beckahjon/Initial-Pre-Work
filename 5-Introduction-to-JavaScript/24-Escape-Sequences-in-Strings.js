705f32124dc54cb0fc1c98aaa0328e9b0032049f
Beckah <rebekah.whittaker19@gmail.com>
2019-04-19 17:03:07 -0600

    Use escape characters to create multiple lines

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,2 @@
+//Escaping characters allow you to do things like use characters you might not otherwise be able to type out, or to represent multiple quotes in a string and allow JavaScript to interpret them correctly.
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
