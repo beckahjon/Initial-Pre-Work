a351ac39cbb35a0b848393c5d5305244868eca92
Beckah <rebekah.whittaker19@gmail.com>
2019-04-19 17:01:27 -0600

    Create string literal with quotes

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,8 @@
+//When a series of zero or more characters are enclosed in single or double quotation marks, it is called a string literal.
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Beckah";
+var myLastName = "Whittaker";
