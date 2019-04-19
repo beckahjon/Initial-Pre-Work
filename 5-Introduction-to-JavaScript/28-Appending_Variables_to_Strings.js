d5b451fe0e69990997353e702d5ab94875bbabf2
Beckah <rebekah.whittaker19@gmail.com>
2019-04-19 17:07:38 -0600

    Use += operator to attach variable to existing string

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,11 @@
+//You can attach variables to a string using the += operator.
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "sparking my imagination!";
+var myStr = "Learning to code is ";
+myStr += someAdjective; 
