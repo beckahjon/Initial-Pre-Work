0ad9fdf7f79b26d27c838f02f00f824deb7ded13
Beckah <rebekah.whittaker19@gmail.com>
2019-04-19 10:32:13 -0600

    Define variables of a, b, and c

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,11 @@
+//When variables are declared, they have an initial value of undefined. You can't do a mathematical operation on an undefined variable. If you link a string with an undefined variable you will get a strong of undefined. If you initialize the variables they will not come up as undefined.
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
