1032043cfe560a2bc2105c2904db12219d07cd37
Beckah <rebekah.whittaker19@gmail.com>
2019-04-19 10:57:47 -0600

    Use += operator to add and assign variables in one step

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,10 @@
+//You can use assignments to modify the contents of a variable. There is an operator of plus sign then equal sign to do both a mathematical operation and assignment in one step.
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
