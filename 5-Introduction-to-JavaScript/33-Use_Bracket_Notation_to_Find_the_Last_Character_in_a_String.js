2a99a803e2faef408cd5c009a563b47faa61a29b
Beckah <rebekah.whittaker19@gmail.com>
2019-04-20 12:45:39 -0600

    Use .length and subtract 1 to find last character within string

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,10 @@
+//If you want to find the last character of a string, you can use .length and subtract one.
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
