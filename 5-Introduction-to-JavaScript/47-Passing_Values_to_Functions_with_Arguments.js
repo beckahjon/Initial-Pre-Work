18933614f37d97b6cabdde31ba57c99928b1bde6
Beckah <rebekah.whittaker19@gmail.com>
2019-04-20 13:23:59 -0600

    Add arguments to function

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,12 @@
+//I learned that parameters are variables that act as placeholders within a function. A function is defined along with one or more parameters. The values that are input, or passed, into a function are called arguments.
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(a, b) {
+  console.log(a + b);
+}
+functionWithArgs(13, 45);
