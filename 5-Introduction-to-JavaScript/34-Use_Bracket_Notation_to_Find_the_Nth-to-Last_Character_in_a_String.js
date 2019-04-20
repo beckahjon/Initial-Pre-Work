47ba39d8b661d375f65c9571853d62eafbd3f132
Beckah <rebekah.whittaker19@gmail.com>
2019-04-20 12:46:14 -0600

    Use .length and minus nth number to find specific character within string

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,10 @@
+//You can also use bracket notation to retrieve any character in a string by using .length and then minus the nth-to-last character.
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
