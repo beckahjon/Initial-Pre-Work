9e904b60babdb3a30e0cf25dcb750352362c4245
Beckah <rebekah.whittaker19@gmail.com>
2019-04-20 13:23:07 -0600

    Create list using arrays within an array

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,9 @@
+//You can create lists by using multi-dimensional arrays.
+var myList = [
+    ["Apples", 3],
+    ["Carrots", 10],
+    ["Eggs", 12],
+    ["Frozen pizza", 2],
+    ["Yogurt", 5],
+    ["Bar soap", 2],
+    ];
