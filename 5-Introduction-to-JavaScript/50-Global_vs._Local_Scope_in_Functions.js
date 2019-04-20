24f216f17b33e309cfd932d79f06b47169d59258
Beckah <rebekah.whittaker19@gmail.com>
2019-04-20 13:25:16 -0600

    Add local variable to take precedence over global variable

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,14 @@
+//If you have both local and global variables with the same name, the local takes precedence over the global.
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
