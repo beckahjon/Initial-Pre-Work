bcfb4342c2f5a926888ccbef5ad2f58ef9ebdaaa
Beckah <rebekah.whittaker19@gmail.com>
2019-04-20 12:47:08 -0600

    Use array declaration

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,6 @@
+//I learned you can store multiple pieces of data in one place by using an array variable. An array declaration begins and ends with a square bracket, and each entry inside is separated by a comma.
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Carrot cake", 42];
