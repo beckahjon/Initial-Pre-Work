fd02d0d912ad51c1ea4aa44c9883cb402690f570
Beckah <rebekah.whittaker19@gmail.com>
2019-04-20 13:23:33 -0600

    Add and call function

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,13 @@
+//Functions are reusable parts. You can call or invoke any function by using its name followed by parenthesis. Each time the function is called all the code between the curly braces will be executed.
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction () {
+    console.log("Hi World");
+}
+reusableFunction();
