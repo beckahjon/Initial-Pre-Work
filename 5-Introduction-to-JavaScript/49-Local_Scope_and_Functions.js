26b1e2d0b2257ba381e6f1b079a6057500627633
Beckah <rebekah.whittaker19@gmail.com>
2019-04-20 13:24:46 -0600

    Add variable with local scope

---
+++ 5-Introduction-to-JavaScript/49-Local_Scope_and_Functions.js
@@ -0,0 +1,13 @@
+//Variables declared within a function, and the function parameters, have local scope, which means they are only visible within that function.
+function myLocalScope() {
+  'use strict'; // you shouldn't need to edit this line
+  var myVar
+  console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+
+
+// Now remove the console log line to pass the test
