ac79ac415d05090246f9e0c6a93509b176e3a3d6
Beckah <rebekah.whittaker19@gmail.com>
2019-04-20 13:22:16 -0600

    Add pop function

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,11 @@
+//I learned you can "pop" a value off the end of an array by using the .pop() function. You can store the popped off value by assigning it to a variable.
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
