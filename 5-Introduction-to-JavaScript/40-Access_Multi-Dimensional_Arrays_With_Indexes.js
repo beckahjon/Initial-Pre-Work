09d1be6eabf459f984dfbb05397276336c750090
Beckah <rebekah.whittaker19@gmail.com>
2019-04-20 12:48:55 -0600

    Find data within multi-dimensional array

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,6 @@
+//A multi-dimensional array is essentially an array of arrays. The first set of brackets refers to the entries in the outer most array, and each subsequent set of brackets refers to the next level of entries.
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
