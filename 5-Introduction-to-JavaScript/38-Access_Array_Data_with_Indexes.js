7b10211aa7acf18424013effe974f3eea341bfa2
Beckah <rebekah.whittaker19@gmail.com>
2019-04-20 12:48:10 -0600

    Use bracket notation as index to find data within array

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,10 @@
+//I learned that you can access data within arrays by using indexes. They are also written in bracket notation but they are specifying an entry in an array. They also use zero-based indexing.
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0]
