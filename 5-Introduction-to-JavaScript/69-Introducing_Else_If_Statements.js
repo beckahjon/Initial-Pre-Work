0e40917c12ca9e101d985e7c66fdc0d0742a5a28
Beckah <rebekah.whittaker19@gmail.com>
2019-04-21 18:47:02 -0600

    Add else if statements

---
+++ 5-Introduction-to-JavaScript/69-Introducing_Else_If_Statements.js
@@ -0,0 +1,17 @@
+//I  learned you can chain if statements together with else if statements if you have multiple conditions to address.
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  else if (val < 5) {
+    return "Smaller than 5";
+  }
+
+  else {
+    return "Between 5 and 10";
+}
+}
+
+// Change this value to test
+testElseIf(7);
