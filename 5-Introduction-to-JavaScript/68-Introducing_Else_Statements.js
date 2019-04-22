f530cd829095c875871fbba89e6d82ffb488d632
Beckah <rebekah.whittaker19@gmail.com>
2019-04-21 18:46:45 -0600

    Add else statement

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,17 @@
+//If you want something to happen even if your code would return false, you can add an else statment which would then be executed.
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
