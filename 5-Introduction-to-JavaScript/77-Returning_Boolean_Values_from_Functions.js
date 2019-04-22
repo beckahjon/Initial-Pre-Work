8b3f1d02e73de425db86860996678330c02a3e1a
Beckah <rebekah.whittaker19@gmail.com>
2019-04-21 21:55:07 -0600

    Use === operator to return comparison

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+//I learned you can use === to return the result of the comparison instead of using an if/else statement.
+function isLess(a, b) {
+  // Fix this code
+  return (a < b)
+
+}
+
+// Change these values to test
+isLess(13, 26);
