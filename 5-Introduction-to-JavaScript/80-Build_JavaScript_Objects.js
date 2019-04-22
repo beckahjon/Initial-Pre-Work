7afb294a1d0303d47db481fc354624aa3750f359
Beckah <rebekah.whittaker19@gmail.com>
2019-04-22 12:51:28 -0600

    Create object myDog

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,19 @@
+//I learned that in JavaScript there are objects, in which you access data through properties. You can use strings or numbers as properties, but if your object has any non-string properties, JavaScript will typecast them as strings.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Broccoli",
+  "legs": 6,
+  "tails": 2,
+  "friends": ["The fire hydrant on the corner", "Mrs. Tillerson's cat", "The UPS delivery driver"]
+
+
+};
