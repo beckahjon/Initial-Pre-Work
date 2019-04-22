e70743293354e99541eb1cffff75066e2c6b49a7
Beckah <rebekah.whittaker19@gmail.com>
2019-04-21 18:02:32 -0600

    Change boolean to return true

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,9 @@
+//Booleans are another data type, and they only have two values: true or false. They are never written with quotes.
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
