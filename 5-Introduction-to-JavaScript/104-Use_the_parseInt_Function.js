2c4c92a74eaefd05e4ee63b6b047e9d34196d787
Beckah <rebekah.whittaker19@gmail.com>
2019-04-23 11:25:15 -0600

    Use parseInt function

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,7 @@
+//There is a function called parseInt() that parses a string and returns an integer. If the first character of a string can't be converted to a number, it returns NaN.
+function convertToInteger(str) {
+var a = parseInt (str)
+return a;
+}
+
+convertToInteger("Hamlet");
