5580af4c99b33fa0a59fedf3ab18e629eb404a1a
Beckah <rebekah.whittaker19@gmail.com>
2019-04-22 21:40:48 -0600

    Add for loop to run code for specified number of times

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,15 @@
+//A "for loop" is the most common type of loop. It runs for a specfic number of times. There are three optional expressions: initialization (executed one time only before the loop starts), condition (continues as long as the condition is true), and final-expression (executed at the end of each loop and can be used to increment or decrement the loop counter).
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 6; i++) {
+    myArray.push(i);
+}
