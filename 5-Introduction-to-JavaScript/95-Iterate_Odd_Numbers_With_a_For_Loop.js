23ef34194e4c1be2411616ec8ad854eb1a8e540b
Beckah <rebekah.whittaker19@gmail.com>
2019-04-22 21:41:09 -0600

    Find odd numbers by changing final-expression of for loop

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,15 @@
+//You can change your final-expression so you don't have to iterate one for loop at a time.
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 10; i += 2) {
+  myArray.push(i)
+}
