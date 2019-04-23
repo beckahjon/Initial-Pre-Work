f7b9916fa74aa5ce8c8edf6e94c056b9ae48d43a
Beckah <rebekah.whittaker19@gmail.com>
2019-04-22 21:39:30 -0600

    Access nested object using dot and bracket notation

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,15 @@
+//I learned you can access nested objects by chaining together the dot or bracket notation.
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
