f76073f6079a3276edd1cdb37458685712701c55
Beckah <rebekah.whittaker19@gmail.com>
2019-04-22 21:39:46 -0600

    Access nested array using array bracket notation

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,24 @@
+//You can access nested arrays by using array bracket notation.
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
