e554c95c1aafeb6bc14c8ca08c47dbd200905317
Beckah <rebekah.whittaker19@gmail.com>
2019-04-22 21:39:08 -0600

    Add data to object using flexible data structure

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,25 @@
+//JavaScript allows you to store data in a flexible data structure by using an object. They allow for arbitrary combinations of everything from booleans to numbers. JSON, or JavaScript Object Notation, is a data interchange format used to store data.
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+   {
+    "artist": "Elton John",
+    "title": "Rocket Man",
+    "release_year": 1972,
+    "formats": [
+      "vinyl",
+      "CD"
+    ],
+    "gold": true
+  }
+
+];
