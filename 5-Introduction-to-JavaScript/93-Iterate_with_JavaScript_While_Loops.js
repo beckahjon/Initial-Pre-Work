cd5c4259028b6aba1681bf566226b9c72630dc1f
Beckah <rebekah.whittaker19@gmail.com>
2019-04-22 21:40:14 -0600

    Use while loop to run code multiple times

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,10 @@
+//A loop allows you to run the same code multiple times. A while loop runs while a condition is true and stops when the condition is no longer true.
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while(i < 5) {
+    myArray.push(i)
+    i++;
+}
