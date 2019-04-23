34e832c6ba5269ef0e3bc4c43071b4954fe51766
Beckah <rebekah.whittaker19@gmail.com>
2019-04-22 21:35:32 -0600

    Use bracket notation to access property stored as value

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,12 @@
+//You can use bracket notation on objects to access a property that is stored as the value of a variable. Do not use quotes, as you are using the value, not the name, of the variable.
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
