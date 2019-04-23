8e5bbeb6bae42d5b002b227ea988e13407313bd3
Beckah <rebekah.whittaker19@gmail.com>
2019-04-23 11:25:35 -0600

    Use parseInt function with radix

---
+++ 5-Introduction-to-JavaScript/105-Use_the_parseInt_Function_with_a_Radix.js
@@ -0,0 +1,6 @@
+//You can also use a second argument for the radix when you use the parseInt function to return an integer.
+function convertToInteger(str) {
+  return parseInt(str, 2)
+}
+
+convertToInteger("01101");
