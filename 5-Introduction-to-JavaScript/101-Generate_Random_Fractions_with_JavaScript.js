9b2d97ef4746ce048c831ba918765a9556bdd3d6
Beckah <rebekah.whittaker19@gmail.com>
2019-04-23 11:19:38 -0600

    Generate random decimal with Math.random

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,11 @@
+//You can use the Math.random function to generate a random decimal number between 0 and 1. You can use a while loop with the Math.random function to generate random numbers while your variable result is a specified value.
+function randomFraction() {
+
+  // Only change code below this line.
+    var result = 0;
+   while (result === 0) {
+       result = Math.random()
+   }
+   return result;
+  // Only change code above this line.
+}
