619130a1b67a8a3c9dde18b24e70ed7c6e3912bb
Beckah <rebekah.whittaker19@gmail.com>
2019-04-23 11:26:24 -0600

    Use conditional operator instead of if/else expression

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,6 @@
+//I learned you can use a conditional (or ternary) operator as an if-else expression on one line.
+function checkEqual(a, b) {
+return a === b ? true : false;
+}
+
+checkEqual(2, 1);
