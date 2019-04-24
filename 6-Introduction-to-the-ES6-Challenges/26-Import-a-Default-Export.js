da5e789ddbcd6e755e61eca2acf94a61c954798e
Beckah <rebekah.whittaker19@gmail.com>
2019-04-23 21:31:16 -0600

    Import default export using proper syntax

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,4 @@
+//You can import a default export, but it requires a different syntax. The imported value is not put into curly braces; instead, you simply write the value's name after import.
+"use strict";
+subtract(7,4);
+import subtract from "math_functions";
