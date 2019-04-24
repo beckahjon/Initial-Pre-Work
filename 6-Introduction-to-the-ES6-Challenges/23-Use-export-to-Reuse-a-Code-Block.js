76c4fd3814c2598ebff6fff60779edca752f2746
Beckah <rebekah.whittaker19@gmail.com>
2019-04-23 21:30:06 -0600

    Use export to make code usable elsewhere

---
+++ 6-Introduction-to-the-ES6-Challenges/23-Use-export-to-Reuse-a-Code-Block.js
@@ -0,0 +1,4 @@
+//Export is another feature you can use if you want a part of your code to be usable in another file. This is also a non-browser feature.
+"use strict";
+export const foo = "bar";
+export const bar = "foo";
