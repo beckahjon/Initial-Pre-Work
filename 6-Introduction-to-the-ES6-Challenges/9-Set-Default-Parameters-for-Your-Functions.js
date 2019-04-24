2345895e5cead631e6d4fe33a1820e9d54b35597
Beckah <rebekah.whittaker19@gmail.com>
2019-04-23 21:18:54 -0600

    Add default parameter to function

---
+++ 6-Introduction-to-the-ES6-Challenges/9-Set-Default-Parameters-for-Your-Functions.js
@@ -0,0 +1,9 @@
+//I learned ES6 allows you add default parameters to functions that will execute if the argument is not specified.
+const increment = (function() {
+  "use strict";
+  return function increment(number, value = 1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
