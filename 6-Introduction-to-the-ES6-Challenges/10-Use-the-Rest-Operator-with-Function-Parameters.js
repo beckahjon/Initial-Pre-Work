f03d5ae0d4d592f0c38446596a7ce159cd274908
Beckah <rebekah.whittaker19@gmail.com>
2019-04-23 21:22:10 -0600

    Add rest operator to create function to take variable number of arguments

---
+++ 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -0,0 +1,8 @@
+//You can use the rest operator to create functions that take a variable number of arguments. These arguments are stored in an array that can be accessed later from inside the function.
+const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
