2cbbfd51a997792d77285d52a0327a25aa2c82b2
Beckah <rebekah.whittaker19@gmail.com>
2019-04-23 21:17:29 -0600

    Use arrow syntax to create in-line function

---
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -0,0 +1,5 @@
+//I learned you can create in-line functions, and you don't have to name them because you don't use them anywhere else. You can use arrow function syntax instead.
+const magic = () => {
+  "use strict";
+  return new Date();
+};
